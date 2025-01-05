5cbd5bed8ae68a0f7a016579e20713c7106d5169
John Moore <pinkushin@verizon.net>
2025-01-05 13:06:16 -0500

    Add second test case testing for proper null input and add more checks to NameChanged test

--- PokemonBattleJournal.Tests/ViewModels/MainPageViewModelTests.cs
+++ PokemonBattleJournal.Tests/ViewModels/MainPageViewModelTests.cs
@@ -7,15 +7,29 @@
         {
             // Arrange
             var viewModel = new MainPageViewModel();
-            string? nameInput = "TestName";
-            
-
+            string nameInput = "TestName";
             // Act
             viewModel.NameInput = nameInput;
             viewModel.UpdateTrainerName();
-
             // Assert
             viewModel.TrainerName.Should().Be(nameInput);
+            viewModel.WelcomeMsg.Should().Be($"Welcome {nameInput}");
+            viewModel.NameInput.Should().BeNull();
+        }
+        [Fact]
+        public void MainPageViewmodel_UpdateTrainerName_TrainerNameUnchanged()
+        {
+            // Arrange
+            var viewModel = new MainPageViewModel();
+            string? nameInput = null;
+            var prevName = viewModel.TrainerName;
+            // Act
+            viewModel.NameInput = nameInput;
+            viewModel.UpdateTrainerName();
+            // Assert
+            viewModel.TrainerName.Should().NotBe(nameInput);
+            viewModel.TrainerName.Should().Be(prevName);
+            viewModel.WelcomeMsg.Should().Be($"Welcome {prevName}");
         }
     }
 }
