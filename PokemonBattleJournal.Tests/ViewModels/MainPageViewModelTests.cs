b25493fb6255957220d8d8dc0fffc87a685e1cb7
John Moore <pinkushin@verizon.net>
2025-01-05 09:59:25 -0500

    create xunit testing project and setup app for testing

---
+++ PokemonBattleJournal.Tests/ViewModels/MainPageViewModelTests.cs
@@ -0,0 +1,24 @@
+﻿using PokemonBattleJournal.ViewModels;
+using System;
+using Xunit;
+
+namespace PokemonBattleJournal.Tests.ViewModels
+{
+    public class MainPageViewModelTests
+    {
+        [Fact]
+        public void UpdateTrainerName_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var viewModel = new MainPageViewModel();
+            string? trainerName = null;
+
+            // Act
+            viewModel.UpdateTrainerName(
+                trainerName);
+
+            // Assert
+            Assert.True(false);
+        }
+    }
+}
