52e3415656d7128235f162533b37fccd98972f92
John Moore <pinkushin@verizon.net>
2025-01-05 13:04:25 -0500

    Add MainThreadHelper for unit testing

---
+++ PokemonBattleJournal/Utilities/MainThreadHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonBattleJournal.Utilities
+{
+    public static class MainThreadHelper
+    {
+        public static void BeginInvokeOnMainThread(Action action)
+        {
+            //This is a wrapper for supporting unit tests
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                Task.Run(action);
+                return;
+            }
+            MainThread.BeginInvokeOnMainThread(action);
+        }
+
+        public static bool IsMainThread
+        {
+            get
+            {
+                //This is a wrapper for supporting unit tests
+                if (DeviceInfo.Platform == DevicePlatform.Unknown)
+                {
+                    return true;
+                }
+                return MainThread.IsMainThread;
+            }
+        } 
+    }
+}
