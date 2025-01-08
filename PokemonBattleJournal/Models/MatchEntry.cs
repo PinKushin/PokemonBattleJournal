162e8f5c788b95854c9a92c890c1098267f01937
John Moore <pinkushin@verizon.net>
2025-01-08 07:11:28 -0500

    Add match entry model

---
+++ PokemonBattleJournal/Models/MatchEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace PokemonBattleJournal.Model
+{
+    [JsonObject]
+    public class MatchEntry
+    {
+        public string PlayerSelected = "Other";
+        public string RivalSelected = "Other";
+        public TimeSpan StartTime;
+        public TimeSpan EndTime;
+        public DateTime DatePlayed;
+        public bool FirstCheck;
+        public string Result = "";
+        public IList<string> TagsSelected = [];
+        public string Note = "";
+    }
+}
