42a7a761c4eb615104bc2b66f679b640a350b649
John Moore <pinkushin@verizon.net>
2025-01-08 13:08:47 -0500

    Add TcgDexCard and Brief models and create service file to get cards from the TcgDexApi.

---
+++ PokemonBattleJournal/Models/TcgCardBrief.cs
@@ -0,0 +1,28 @@
+﻿// Ignore Spelling: Tcg
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PokemonBattleJournal.Models
+{
+    public class TcgDexCardBrief
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = "0";
+
+        [JsonPropertyName("localId")]
+        public string LocalId { get; set; } = "0";
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = "Card Name";
+
+        [JsonPropertyName("image")]
+        public string Image { get; set; } = "card_backside_atomicmonkeytcg.png";
+
+        public TcgDexCardBrief() { }
+    }
+}
---
+++ PokemonBattleJournal/Services/TcgDexApiService.cs
@@ -0,0 +1,24 @@
+﻿// Ignore Spelling: Api Tcg
+
+using System.Net.Http.Json;
+
+namespace PokemonBattleJournal.Services
+{
+    public class TcgDexApiService
+    {
+        public HttpClient? HttpClient;
+
+        public TcgDexApiService()
+        {
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet) return;
+            HttpClient = new HttpClient();
+        }
+
+        public async Task<TcgDexCardBriefCollection?> AllCardsAsync()
+        {
+            if (HttpClient == null) HttpClient = new();
+            TcgDexCardBriefCollection? json = await HttpClient.GetFromJsonAsync<TcgDexCardBriefCollection>("https://api.tcgdex.net/v2/en/cards");
+            return json;
+        }
+    }
+}
