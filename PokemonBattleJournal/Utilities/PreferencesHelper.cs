dfebf0124a8237c8edcdd99cf60cbac381c383f0
John Moore <pinkushin@verizon.net>
2025-01-08 08:07:46 -0500

    Create preferences helper to save to preferences only when not unit testing.

---
+++ PokemonBattleJournal/Utilities/PreferencesHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonBattleJournal.Utilities
+{
+    internal class PreferencesHelper
+    {
+        public string GetSetting(string key)
+        {
+            //This is a wrapper for supporting unit tests
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                return "Trainer";
+            }
+            return Preferences.Get(key, "Trainer");
+        }
+
+        public void SetSetting(string key, string value)
+        {
+            //This is a wrapper for supporting unit tests
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                return;
+            }
+            Preferences.Set(key, value);
+        }
+    }
+}
