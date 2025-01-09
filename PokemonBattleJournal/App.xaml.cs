b2bbfdb47a2ba7a1bd9733f7372ca27a7d046f42
John Moore <pinkushin@verizon.net>
2025-01-09 10:27:05 -0500

    Comment App.CreateWindow

--- PokemonBattleJournal/App.xaml.cs
+++ PokemonBattleJournal/App.xaml.cs
@@ -5,12 +5,14 @@
         public App()
         {
             InitializeComponent();
-             
+            
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
+            // It can also be defined as an Object Initializer
+            //return new Window() { Page = new AppShell() };
         }
     }
 }
