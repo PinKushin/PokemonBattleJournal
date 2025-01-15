2698ab5a65a9134915a563e7f07b15b5f41662ca
John Moore <pinkushin@verizon.net>
2025-01-15 15:36:19 -0500

    Refactor Tests

--- PokemonBattleJournal.Tests/ViewModels/MainPageViewModelTests.cs
+++ PokemonBattleJournal.Tests/ViewModels/MainPageViewModelTests.cs
@@ -2,34 +2,42 @@
 {
     public class MainPageViewModelTests
     {
+        private readonly MainPageViewModel _viewModel;
+        public MainPageViewModelTests() 
+        {
+            //SUT
+            _viewModel = new MainPageViewModel();
+        }
+        //naming convention = ClassName__MethodName_ExpectedResult
         [Fact]
         public void MainPageViewModel_UpdateTrainerName_TrainerNameChanged()
         {
             // Arrange
-            var viewModel = new MainPageViewModel();
             string nameInput = "TestName";
             // Act
-            viewModel.NameInput = nameInput;
-            viewModel.UpdateTrainerName();
+            _viewModel.NameInput = nameInput;
+            _viewModel.UpdateTrainerName();
             // Assert
-            viewModel.TrainerName.Should().Be(nameInput);
-            viewModel.WelcomeMsg.Should().Be($"Welcome {nameInput}");
-            viewModel.NameInput.Should().BeNull();
+            _viewModel.TrainerName.Should().Be(nameInput);
+            _viewModel.WelcomeMsg.Should().Be($"Welcome {nameInput}");
+            _viewModel.NameInput.Should().BeNull();
         }
+
         [Fact]
         public void MainPageViewModel_UpdateTrainerName_TrainerNameUnchanged()
-        {
+        {//naming convention = ClassName__MethodName_ExpectedResult
+
             // Arrange
-            var viewModel = new MainPageViewModel();
             string? nameInput = null;
-            var prevName = viewModel.TrainerName;
+            var prevName = _viewModel.TrainerName;
             // Act
-            viewModel.NameInput = nameInput;
-            viewModel.UpdateTrainerName();
+            _viewModel.NameInput = nameInput;
+            _viewModel.UpdateTrainerName();
             // Assert
-            viewModel.TrainerName.Should().NotBe(nameInput);
-            viewModel.TrainerName.Should().Be(prevName);
-            viewModel.WelcomeMsg.Should().Be($"Welcome {prevName}");
+            _viewModel.TrainerName.Should().NotBe(nameInput);
+            _viewModel.TrainerName.Should().Be(prevName);
+            _viewModel.WelcomeMsg.Should().Be($"Welcome {prevName}");
+            _viewModel.NameInput.Should().BeNull();
         }
     }
 }
