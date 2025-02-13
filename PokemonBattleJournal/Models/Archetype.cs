8f0e10ff04ea7aeac356ce46c81e3ed7c8a6c51b
John Moore <PinKushin@verizon.net>
2025-02-13 15:55:50 -0500

    Create Archetype Class and Interface

---
+++ PokemonBattleJournal/Models/Archetype.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using PokemonBattleJournal.Models.Interfaces;
+
+namespace PokemonBattleJournal.Models
+{
+	public class Archetype : IArchetype
+	{
+		public Archetype(string name, string imagePath = "ball_icon.png")
+		{
+			Name = name;
+			ImagePath = imagePath;
+		}
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+		[JsonProperty("imagePath")]
+		public string ImagePath { get; set; } = "ball_icon.png";
+
+		public override bool Equals(object? obj)
+		{
+			return obj is IArchetype archetype &&
+				   Name == archetype.Name;
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Name);
+		}
+
+		public static bool operator ==(Archetype? left, Archetype? right)
+		{
+			return EqualityComparer<Archetype>.Default.Equals(left, right);
+		}
+
+		public static bool operator !=(Archetype? left, Archetype? right)
+		{
+			return !(left == right);
+		}
+	}
+}
---
+++ PokemonBattleJournal/Models/Interfaces/IArchetype.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace PokemonBattleJournal.Models.Interfaces
+{
+	public interface IArchetype
+	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
+	}
+}
