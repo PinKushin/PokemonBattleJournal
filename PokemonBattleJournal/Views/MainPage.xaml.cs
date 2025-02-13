4a24781a70e433637ae0a5f2e1f88245e0210c40
John Moore <PinKushin@verizon.net>
2025-02-13 16:07:43 -0500

    Add Syncfusion and CommunityToolkit components and helpers

--- PokemonBattleJournal/Views/MainPage.xaml.cs
+++ PokemonBattleJournal/Views/MainPage.xaml.cs
@@ -6,7 +6,23 @@
         {
             InitializeComponent();
             BindingContext = vm;
+			if (!OperatingSystem.IsWindows())
+			{
+				//RightColumn.Rotation = 90;
+			}
         }
-    }
+
+		private void OpenTimePickers(object sender, EventArgs e)
+		{
+			
+			EndTimePicker.IsOpen = true;
+			StartTimePicker.IsOpen = true;
+		}
+
+		private void OpenDatePlayedPicker(object sender, EventArgs e)
+		{
+			DatePlayedPicker.IsOpen = true;
+		}
+	}
 
 }
