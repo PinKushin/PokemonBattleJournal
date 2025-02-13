1a142e940cc2522ac3690531c6d2440c85f3af3f
John Moore <PinKushin@verizon.net>
2025-02-13 16:06:43 -0500

    Add Sentry to project

--- PokemonBattleJournal/MauiProgram.cs
+++ PokemonBattleJournal/MauiProgram.cs
@@ -5,78 +5,92 @@ using Microsoft.Extensions.Logging;
 using Syncfusion.Maui.Toolkit.Hosting;
 namespace PokemonBattleJournal
 {
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			MauiAppBuilder builder = MauiApp.CreateBuilder();
+			builder
 				.ConfigureSyncfusionToolkit()
-                .UseMauiApp<App>()
-            .ConfigureSyncfusionCore()
-                .UseMauiCommunityToolkit()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				.UseMauiApp<App>()
+				.ConfigureSyncfusionCore()
+				.UseMauiCommunityToolkit()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 
-                    fonts.AddFont("Saira-Regular.ttf", "SairaRegular");
-                    fonts.AddFont("Saira-Bold.ttf", "SairaBold");
-                    fonts.AddFont("Saira-Black.ttf", "SairaBlack");
-                  
-                    fonts.AddFont("Charm-Regular.ttf", "CharmRegular");
-                    fonts.AddFont("Charm-Bold.ttf", "CharmBold");
+					fonts.AddFont("Saira-Regular.ttf", "SairaRegular");
+					fonts.AddFont("Saira-Bold.ttf", "SairaBold");
+					fonts.AddFont("Saira-Black.ttf", "SairaBlack");
+				  
+					fonts.AddFont("Charm-Regular.ttf", "CharmRegular");
+					fonts.AddFont("Charm-Bold.ttf", "CharmBold");
 
-                    fonts.AddFont("Doto_Rounded-Black.ttf", "DotoRoundedBlack");
+					fonts.AddFont("Doto_Rounded-Black.ttf", "DotoRoundedBlack");
 
-                    fonts.AddFont("PokemonSolid.ttf", "PokemonSolid");
-                    fonts.AddFont("PokemonGb-RAeo.ttf", "PokemonGB");
-                    fonts.AddFont("pokemon_tcg_symbols_font_by_icycatelf_dah5i8h.ttf", "PokemonSymbols");
+					fonts.AddFont("PokemonSolid.ttf", "PokemonSolid");
+					fonts.AddFont("PokemonGb-RAeo.ttf", "PokemonGB");
+					fonts.AddFont("pokemon_tcg_symbols_font_by_icycatelf_dah5i8h.ttf", "PokemonSymbols");
 
-                    fonts.AddFont("FluentSystemIcons-Regular.ttf", FluentUI.FontFamily);
-                });
-			
-            #if DEBUG
-    		builder.Logging.AddDebug();
-            builder.Services.AddLogging(configure => configure.AddDebug());
-            #endif
-            //Link Pages and ViewModels
-            //Main Page
-            builder.Services.AddSingleton<MainPage>();
-            builder.Services.AddSingleton<MainPageViewModel>();
-
-            //Read Journal Page
-            builder.Services.AddSingleton<ReadJournalPage>();
-            builder.Services.AddSingleton<ReadJournalPageViewModel>();
-
-            //Trainer Page
-            builder.Services.AddSingleton<TrainerPage>();
-            builder.Services.AddSingleton<TrainerPageViewModel>();
-
-            //Options Page
-            builder.Services.AddSingleton<OptionsPage>();
-            builder.Services.AddSingleton<OptionsPageViewModel>();
-
-            //About Page
-            builder.Services.AddSingleton<AboutPage>();
-            builder.Services.AddSingleton<AboutPageViewModel>();
+					fonts.AddFont("FluentSystemIcons-Regular.ttf", FluentUI.FontFamily);
+				})
+				.UseSentry(options =>
+				{
+					options.Dsn = "https://03ff3f5afbfdbbd038a8903bc0e6d5e4@o4508805406851072.ingest.us.sentry.io/4508805409538048";
 
 #if DEBUG
-            //Developer Test Page
-            builder.Services.AddSingleton<TestPage>();
-            builder.Services.AddSingleton<TestPageViewModel>();
+					options.Debug = true;
+					options.TracesSampleRate = 1.0F;
+					options.MaxBreadcrumbs = 1000;
+#else
+					options.Debug = false;
+					options.TracesSampleRate = 0.1;
+					options.MaxBreadcrumbs = 300;
+#endif
+				});
+			
+#if DEBUG
+			builder.Logging.AddDebug();
+			builder.Services.AddLogging(configure => configure.AddDebug());
+#endif
+			//Link Pages and ViewModels
+			//Main Page
+			builder.Services.AddSingleton<MainPage>();
+			builder.Services.AddSingleton<MainPageViewModel>();
+
+			//Read Journal Page
+			builder.Services.AddSingleton<ReadJournalPage>();
+			builder.Services.AddSingleton<ReadJournalPageViewModel>();
+
+			//Trainer Page
+			builder.Services.AddTransient<TrainerPage>();
+			builder.Services.AddTransient<TrainerPageViewModel>();
+
+			//Options Page
+			builder.Services.AddSingleton<OptionsPage>();
+			builder.Services.AddSingleton<OptionsPageViewModel>();
+
+			//About Page
+			builder.Services.AddSingleton<AboutPage>();
+			builder.Services.AddSingleton<AboutPageViewModel>();
+
+#if DEBUG
+			//Developer Test Page
+			builder.Services.AddSingleton<TestPage>();
+			builder.Services.AddSingleton<TestPageViewModel>();
 #endif
 #if WINDOWS
 
-            Microsoft.Maui.Controls.Handlers.Items.CollectionViewHandler.Mapper.AppendToMapping("DisableMultiselectCheckbox", 
-            (handler, view) =>
-            {
-                handler.PlatformView.IsMultiSelectCheckBoxEnabled = false;
-            });
+			Microsoft.Maui.Controls.Handlers.Items.CollectionViewHandler.Mapper.AppendToMapping("DisableMultiselectCheckbox", 
+			(handler, view) =>
+			{
+				handler.PlatformView.IsMultiSelectCheckBoxEnabled = false;
+			});
 
 #endif
 
-            return builder.Build();
-        }
-    }
+			return builder.Build();
+		}
+	}
 }
