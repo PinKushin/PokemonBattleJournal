d9703b53e300c48072fed4926bdefb922a01edef
John Moore <PinKushin@verizon.net>
2025-02-13 16:03:10 -0500

    Add more observable properties and display more data

--- PokemonBattleJournal/ViewModels/ReadJournalPageViewModel.cs
+++ PokemonBattleJournal/ViewModels/ReadJournalPageViewModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Microsoft.Maui.Controls;
 using Newtonsoft.Json;
 
 namespace PokemonBattleJournal.ViewModels
@@ -21,32 +15,62 @@ namespace PokemonBattleJournal.ViewModels
         public partial string WelcomeMsg { get; set; }
         [ObservableProperty]
         public partial string? SelectedNote { get; set; } = "Select Match";
-        [ObservableProperty]
+		[ObservableProperty]
+		public partial string? SelectedNote2 { get; set; } = "Select Match";
+		[ObservableProperty]
+		public partial string? SelectedNote3 { get; set; } = "Select Match";
+		[ObservableProperty]
         public partial MatchEntry? SelectedMatch { get; set; }
-        [ObservableProperty]
-        public partial string NoteHeaderIconSource { get; set; } = "ball_icon.png";
-        [ObservableProperty]
+		[ObservableProperty]
+		public partial string? ResultGame1 { get; set; }
+		[ObservableProperty]
+		public partial string? ResultGame2 { get; set; }
+		[ObservableProperty]
+		public partial string? ResultGame3 { get; set; }
+		[ObservableProperty]
+		public partial string? OverallResult { get; set; }
+		[ObservableProperty]
+		public partial string? PlayingName { get; set; } = "other";
+		[ObservableProperty]
+		public partial string? AgainstName { get; set; } = "other";
+		[ObservableProperty]
+		public partial string? PlayingIconSource { get; set; } = "ball_icon.png";
+		[ObservableProperty]
+		public partial string? AgainstIconSource { get; set; } = "ball_icon.png";
+		[ObservableProperty]
+		public partial List<string>? TagsSelectedGame1  { get; set; }
+		[ObservableProperty]
+		public partial List<string>? TagsSelectedGame2 { get; set; }
+		[ObservableProperty]
+		public partial List<string>? TagsSelectedGame3 { get; set; }
+		[ObservableProperty]
         public partial ObservableCollection<MatchEntry> MatchHistory { get; set; } = new();
 
         public ReadJournalPageViewModel()
         {
             WelcomeMsg = $"{TrainerName}'s Journal";
-            LoadJournalAsync();
+            //MatchHistory = LoadMatchHistory();
 
         }
 
-        private async void LoadJournalAsync()
+		[RelayCommand]
+		public async Task AppearingAsync()
+		{
+			MatchHistory = await LoadMatchHistoryAsync();
+		}
+
+		private async Task<ObservableCollection<MatchEntry>> LoadMatchHistoryAsync()
         {
+            ObservableCollection<MatchEntry> matchHistory = new();
             if (FileHelper.Exists(filePath))
             {
                 //Read File from Disk throws error if file doesn't exist so it was checked above
-                var saveFile = await File.ReadAllTextAsync(filePath);
+                var saveFile = await FileHelper.ReadFileAsync(filePath);
                 //Deserialize file or create an empty list of matches if no matches exist
-                MatchHistory = JsonConvert.DeserializeObject<ObservableCollection<MatchEntry>>(saveFile)
-                    ?? [];
-
+                matchHistory = JsonConvert.DeserializeObject<ObservableCollection<MatchEntry>>(saveFile)
+                    ?? [];                
             }
-
+            return matchHistory;
         }
 
         [RelayCommand]
@@ -54,13 +78,32 @@ namespace PokemonBattleJournal.ViewModels
         {
             if (SelectedMatch == null)
             {
-                NoteHeaderIconSource = "ball_icon.png";
-                return;
+                PlayingIconSource = "ball_icon.png";
+				AgainstIconSource = "ball_icon.png";
+				return;
             }
+			OverallResult = SelectedMatch.Result;
+			ResultGame1 = SelectedMatch.Game1.Result;
+			SelectedNote = SelectedMatch.Game1.Notes;
+			TagsSelectedGame1 = SelectedMatch.Game1.Tags;
 
-            SelectedNote = SelectedMatch.Game1.Notes;
-            
-            NoteHeaderIconSource = $"{SelectedMatch.Playing.Replace(" ", "_")}.png";
-        }
+			if (SelectedMatch.Game2 != null)
+			{
+				ResultGame2 = SelectedMatch.Game2.Result;
+				SelectedNote2 = SelectedMatch.Game2.Notes;
+				TagsSelectedGame2 = SelectedMatch.Game2.Tags;
+			}
+
+			if (SelectedMatch.Game3 != null)
+			{
+				ResultGame2 = SelectedMatch.Game3.Result;
+				SelectedNote2 = SelectedMatch.Game3.Notes;
+				TagsSelectedGame2 = SelectedMatch.Game3.Tags;
+			}
+			PlayingName = SelectedMatch.Playing.Name;
+			AgainstName = SelectedMatch.Against.Name;
+			PlayingIconSource = SelectedMatch.Playing.ImagePath;
+			AgainstIconSource = SelectedMatch.Against.ImagePath;
+		}
     }
 }
