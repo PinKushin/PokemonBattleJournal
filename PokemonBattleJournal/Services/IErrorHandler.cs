ada565ca6e7919ed2b4fb24d4e1d9e0214bd4964
PinKushin <pinkushin@verizon.net>
2025-02-15 14:54:21 -0500

    Remove unneeded Usings

--- PokemonBattleJournal/Services/IErrorHandler.cs
+++ PokemonBattleJournal/Services/IErrorHandler.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PokemonBattleJournal.Services
+﻿namespace PokemonBattleJournal.Services
 {
-    /// <summary>
-    /// Error Handler Service.
-    /// </summary>
-    public interface IErrorHandler
-    {
-        /// <summary>
-        /// Handle error in UI.
-        /// </summary>
-        /// <param name="ex">Exception being thrown.</param>
-        void HandleError(Exception ex);
-    }
+	/// <summary>
+	/// Error Handler Service.
+	/// </summary>
+	public interface IErrorHandler
+	{
+		/// <summary>
+		/// Handle error in UI.
+		/// </summary>
+		/// <param name="ex">Exception being thrown.</param>
+		void HandleError(Exception ex);
+	}
 }
