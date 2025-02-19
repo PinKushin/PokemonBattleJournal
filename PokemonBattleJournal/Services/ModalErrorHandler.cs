99f5e86f0e654bf667d3e5d2a5cc8a78801d60ae
PinKushin <pinkushin@verizon.net>
2025-02-19 05:53:39 -0500

    reformat and rename to include async

--- PokemonBattleJournal/Services/ModalErrorHandler.cs
+++ PokemonBattleJournal/Services/ModalErrorHandler.cs
@@ -1,39 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PokemonBattleJournal.Services
+﻿namespace PokemonBattleJournal.Services
 {
-    /// <summary>
-    /// Modal Error Handler.
-    /// </summary>
-    public class ModalErrorHandler : IErrorHandler
-    {
-        private static readonly SemaphoreSlim _semaphore = new(1, 1);
+	/// <summary>
+	/// Modal Error Handler.
+	/// </summary>
+	public class ModalErrorHandler : IErrorHandler
+	{
+		private static readonly SemaphoreSlim _semaphore = new(1, 1);
 
-        /// <summary>
-        /// Handle error in UI.
-        /// </summary>
-        /// <param name="ex">Exception.</param>
-        public void HandleError(Exception ex)
-        {
-            DisplayAlert(ex).FireAndForgetSafeAsync();
-        }
+		/// <summary>
+		/// Handle error in UI.
+		/// </summary>
+		/// <param name="ex">Exception.</param>
+		public void HandleError(Exception ex)
+		{
+			DisplayAlertAsync(ex).FireAndForgetSafeAsync();
+		}
 
-        static async Task DisplayAlert(Exception ex)
-        {
-            try
-            {
-                await _semaphore.WaitAsync();
-                if (Shell.Current is Shell shell)
-                    await shell.DisplayAlert("Error", ex.Message, "OK");
-            }
-            finally
-            {
-                _semaphore.Release();
-            }
-        }
-    }
+		private static async Task DisplayAlertAsync(Exception ex)
+		{
+			try
+			{
+				await _semaphore.WaitAsync();
+				if (Shell.Current is Shell shell)
+					await shell.DisplayAlert("Error", ex.Message, "OK");
+			}
+			finally
+			{
+				_semaphore.Release();
+			}
+		}
+	}
 }
