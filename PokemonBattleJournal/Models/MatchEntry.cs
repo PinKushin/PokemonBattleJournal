4c34e9ff4d8edd8d1f6fa415dd281b8c31f0b471
PinKushin <pinkushin@verizon.net>
2025-02-19 05:52:59 -0500

    Change DateTimeOffset to Date Time and make Archetypes Nullable

--- PokemonBattleJournal/Models/MatchEntry.cs
+++ PokemonBattleJournal/Models/MatchEntry.cs
@@ -1,56 +1,54 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using PokemonBattleJournal.Models.Interfaces;
 
 namespace PokemonBattleJournal.Models
 {
-    [JsonObject]
-    public partial class MatchEntry
-    {
-        [JsonProperty("playing")]
-        public required Archetype Playing { get; set; }
+	[JsonObject]
+	public partial class MatchEntry
+	{
+		[JsonProperty("playing")]
+		public Archetype? Playing { get; set; }
 
-        [JsonProperty("against")]
-        public required Archetype Against { get; set; }
+		[JsonProperty("against")]
+		public Archetype? Against { get; set; }
 
-        [JsonProperty("time")]
-        public DateTimeOffset Time { get; set; }
+		[JsonProperty("time")]
+		public DateTime Time { get; set; }
 
-        [JsonProperty("result")]
-        public string? Result { get; set; }
+		[JsonProperty("result")]
+		public string? Result { get; set; }
 
 		[JsonProperty("game1")]
 		public Game Game1 { get; set; } = new Game();
 
 		[JsonProperty("game2", NullValueHandling = NullValueHandling.Ignore)]
-        public Game? Game2 { get; set; }
+		public Game? Game2 { get; set; }
 
-        [JsonProperty("game3", NullValueHandling = NullValueHandling.Ignore)]
-        public Game? Game3 { get; set; }
+		[JsonProperty("game3", NullValueHandling = NullValueHandling.Ignore)]
+		public Game? Game3 { get; set; }
 
 		[JsonProperty("start-time", NullValueHandling = NullValueHandling.Ignore)]
-		public DateTimeOffset? StartTime { get; set; }
+		public DateTime StartTime { get; set; }
 
 		[JsonProperty("end-time", NullValueHandling = NullValueHandling.Ignore)]
-		public DateTimeOffset? EndTime { get; set; }
+		public DateTime EndTime { get; set; }
 
 		[JsonProperty("date-played", NullValueHandling = NullValueHandling.Ignore)]
-		public DateTimeOffset? DatePlayed { get; set; }
-
+		public DateTime DatePlayed { get; set; }
 	}
-    public partial class Game
-    {
+
+	public partial class Game
+	{
 		[JsonProperty("result")]
 		public string Result { get; set; } = "Lose";
 
-        [JsonProperty("turn")]
-        
-        public long Turn { get; set; } = 1;
+		[JsonProperty("turn")]
+		public long Turn { get; set; } = 1;
 
-        [JsonProperty("tags")]
-        public List<string> Tags { get; set; } = new();
+		[JsonProperty("tags")]
+		public List<string> Tags { get; set; } = new();
 
-        [JsonProperty("notes")]
-        public string? Notes { get; set; }
-    }
+		[JsonProperty("notes")]
+		public string? Notes { get; set; }
+	}
 }
