61034d04ea21ca6592b5274a011b05e0e3889509
PinKushin <pinkushin@verizon.net>
2025-02-19 05:59:26 -0500

    update global usings

--- PokemonBattleJournal/GlobalUsings.cs
+++ PokemonBattleJournal/GlobalUsings.cs
@@ -1,6 +1,8 @@
-﻿global using PokemonBattleJournal.ViewModels;
-global using PokemonBattleJournal.Views;
+﻿global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using PokemonBattleJournal.Models;
+global using PokemonBattleJournal.Resources.Fonts;
 global using PokemonBattleJournal.Services;
 global using PokemonBattleJournal.Utilities;
-global using PokemonBattleJournal.Resources.Fonts;
-global using PokemonBattleJournal.Models;
+global using PokemonBattleJournal.ViewModels;
+global using PokemonBattleJournal.Views;
