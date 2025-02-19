6be3e1e45e147160525fc7c8e7508731829d7baa
PinKushin <pinkushin@verizon.net>
2025-02-19 05:54:16 -0500

    Make DataPopulationHelper Methods Async

--- PokemonBattleJournal/Utilities/DataPopulationHelper.cs
+++ PokemonBattleJournal/Utilities/DataPopulationHelper.cs
@@ -3,22 +3,22 @@ using Newtonsoft.Json;
 
 namespace PokemonBattleJournal.Utilities
 {
-    public static class DataPopulationHelper
-    {
-		public static ObservableCollection<Archetype> PopulateArchetypes()
+	public static class DataPopulationHelper
+	{
+		public static async Task<ObservableCollection<Archetype>> PopulateArchetypesAsync()
 		{
 			//This is a wrapper for supporting unit tests
 			if (DeviceInfo.Platform == DevicePlatform.Unknown)
 			{
 				return [];
 			}
-			string filePath = FileHelper.GetAppDataPath() + $@"\Archetypes.json";
+			string filePath = FileHelper.GetAppDataPath() + "\\Archetypes.json";
 			ObservableCollection<Archetype>? archetypes = new();
 			if (FileHelper.Exists(filePath))
 			{
-				string? saveFile = File.ReadAllText(filePath);
+				string? saveFile = await FileHelper.ReadFileAsync(filePath);
 				archetypes = JsonConvert.DeserializeObject<ObservableCollection<Archetype>>(saveFile);
-				return archetypes ?? new();
+				return archetypes ?? [];
 			}
 			else
 			{
@@ -31,35 +31,35 @@ namespace PokemonBattleJournal.Utilities
 				archetypes.Add(new Archetype("Miraidon", "miraidon.png"));
 				archetypes.Add(new Archetype("Other", "ball_icon.png"));
 				string saveFile = JsonConvert.SerializeObject(archetypes, Formatting.Indented);
-				File.WriteAllText(filePath, saveFile);
+				await FileHelper.WriteFileAsync(filePath, saveFile);
 				return archetypes;
 			}
 		}
 
-		public static ObservableCollection<string> PopulateTags()
-        {
-            ObservableCollection<string>? tagCollection = new ObservableCollection<string>();
-            string filePath = FileHelper.GetAppDataPath() + $@"\Tags.json";
-            if (!FileHelper.Exists(filePath))
-            {
-                FileHelper.CreateFile(filePath);
-                tagCollection.Add("Early Start");
-                tagCollection.Add("Behind Early");
-                tagCollection.Add("Donked Rival");
-                tagCollection.Add("Got Donked");
-                tagCollection.Add("Lucky");
-                tagCollection.Add("Unlucky");
-                tagCollection.Add("Never Punished");
-                tagCollection.Add("Punished");
-                string saveFile = JsonConvert.SerializeObject(tagCollection);
-                File.WriteAllText(filePath, saveFile);
-            }
-            else
-            {
-                string savedTags = File.ReadAllText(filePath);
-                tagCollection = JsonConvert.DeserializeObject<ObservableCollection<string>>(savedTags);
-            }          
-            return tagCollection ?? new();
-        }
-    }
+		public static async Task<ObservableCollection<string>> PopulateTagsAsync()
+		{
+			ObservableCollection<string> tagCollection = [];
+			string filePath = FileHelper.GetAppDataPath() + "\\Tags.json";
+			if (!FileHelper.Exists(filePath))
+			{
+				FileHelper.CreateFile(filePath);
+				tagCollection.Add("Early Start");
+				tagCollection.Add("Behind Early");
+				tagCollection.Add("Donked Rival");
+				tagCollection.Add("Got Donked");
+				tagCollection.Add("Lucky");
+				tagCollection.Add("Unlucky");
+				tagCollection.Add("Never Punished");
+				tagCollection.Add("Punished");
+				string saveFile = JsonConvert.SerializeObject(tagCollection, Formatting.Indented);
+				await FileHelper.WriteFileAsync(filePath, saveFile);
+			}
+			else
+			{
+				string savedTags = await FileHelper.ReadFileAsync(filePath);
+				tagCollection = JsonConvert.DeserializeObject<ObservableCollection<string>>(savedTags) ?? [];
+			}
+			return tagCollection;
+		}
+	}
 }
