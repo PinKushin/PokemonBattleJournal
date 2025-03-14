321c5596089f688f55e1967b64fef11e1b6ab422
PinKushin <PinKushin@verizon.net>
2025-03-14 02:47:23 -0400

    Refactor MainPage.xaml.cs for code consistency
    Reformatted the `MainPage` class and its methods for improved readability and uniform indentation. No functional changes were made.

--- PokemonBattleJournal/Views/MainPage.xaml.cs
+++ PokemonBattleJournal/Views/MainPage.xaml.cs
@@ -1,22 +1,22 @@
 ﻿namespace PokemonBattleJournal.Views
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage(MainPageViewModel vm)
-		{
-			InitializeComponent();
-			BindingContext = vm;
-		}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(MainPageViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
 
-		private void OpenTimePickers(object sender, EventArgs e)
-		{
-			EndTimePicker.IsOpen = true;
-			StartTimePicker.IsOpen = true;
-		}
+        private void OpenTimePickers(object sender, EventArgs e)
+        {
+            EndTimePicker.IsOpen = true;
+            StartTimePicker.IsOpen = true;
+        }
 
-		private void OpenDatePlayedPicker(object sender, EventArgs e)
-		{
-			DatePlayedPicker.IsOpen = true;
-		}
-	}
+        private void OpenDatePlayedPicker(object sender, EventArgs e)
+        {
+            DatePlayedPicker.IsOpen = true;
+        }
+    }
 }
