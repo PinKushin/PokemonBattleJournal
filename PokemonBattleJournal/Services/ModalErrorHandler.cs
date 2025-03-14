e7fb261004dfbffe0a99b6f782f073f9dd215b36
PinKushin <PinKushin@verizon.net>
2025-03-14 02:23:06 -0400

    Refactor error handling code for readability
    Improved formatting and indentation in `IErrorHandler.cs` and `ModalErrorHandler.cs`. Adjusted comments and method definitions for better clarity while maintaining existing functionality.

--- PokemonBattleJournal/Services/ModalErrorHandler.cs
+++ PokemonBattleJournal/Services/ModalErrorHandler.cs
@@ -1,33 +1,33 @@
 ﻿namespace PokemonBattleJournal.Services
 {
-	/// <summary>
-	/// Modal Error Handler.
-	/// </summary>
-	public class ModalErrorHandler : IErrorHandler
-	{
-		private static readonly SemaphoreSlim _semaphore = new(1, 1);
+    /// <summary>
+    /// Modal Error Handler.
+    /// </summary>
+    public class ModalErrorHandler : IErrorHandler
+    {
+        private static readonly SemaphoreSlim _semaphore = new(1, 1);
 
-		/// <summary>
-		/// Handle error in UI.
-		/// </summary>
-		/// <param name="ex">Exception.</param>
-		public void HandleError(Exception ex)
-		{
-			DisplayAlertAsync(ex).FireAndForgetSafeAsync();
-		}
+        /// <summary>
+        /// Handle error in UI.
+        /// </summary>
+        /// <param name="ex">Exception.</param>
+        public void HandleError(Exception ex)
+        {
+            DisplayAlertAsync(ex).FireAndForgetSafeAsync();
+        }
 
-		private static async Task DisplayAlertAsync(Exception ex)
-		{
-			try
-			{
-				await _semaphore.WaitAsync();
-				if (Shell.Current is Shell shell)
-					await shell.DisplayAlert("Error", ex.Message, "OK");
-			}
-			finally
-			{
-				_ = _semaphore.Release();
-			}
-		}
-	}
+        private static async Task DisplayAlertAsync(Exception ex)
+        {
+            try
+            {
+                await _semaphore.WaitAsync();
+                if (Shell.Current is Shell shell)
+                    await shell.DisplayAlert("Error", ex.Message, "OK");
+            }
+            finally
+            {
+                _ = _semaphore.Release();
+            }
+        }
+    }
 }
