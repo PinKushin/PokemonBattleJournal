8ec79b2a4aa6a2f1e001bf95932a4cbb1e583bff
PinKushin <PinKushin@verizon.net>
2025-03-14 02:43:58 -0400

    Refactor AppShell class formatting
    Reformatted the `AppShell` class in `AppShell.xaml.cs` by removing unnecessary whitespace and improving indentation for better readability. The overall functionality of the class remains unchanged.

--- PokemonBattleJournal/AppShell.xaml.cs
+++ PokemonBattleJournal/AppShell.xaml.cs
@@ -1,10 +1,10 @@
 ﻿namespace PokemonBattleJournal
 {
-	public partial class AppShell : Shell
-	{
-		public AppShell()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
 }
