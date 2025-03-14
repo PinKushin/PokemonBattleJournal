aeb67bdb3e3c8b3df210f68a4b5db9ad8b277a0d
PinKushin <PinKushin@verizon.net>
2025-03-14 02:27:06 -0400

    Refactor PreferencesHelper for improved readability
    Reformatted the `PreferencesHelper` class to enhance consistency and readability. Adjusted indentation and spacing for the `GetSetting` and `SetSetting` methods while preserving their original functionality.

--- PokemonBattleJournal/Utilities/PreferencesHelper.cs
+++ PokemonBattleJournal/Utilities/PreferencesHelper.cs
@@ -1,25 +1,25 @@
 ﻿namespace PokemonBattleJournal.Utilities
 {
-	public static class PreferencesHelper
-	{
-		public static string GetSetting(string key)
-		{
-			//This is a wrapper for supporting unit tests
-			if (DeviceInfo.Platform == DevicePlatform.Unknown)
-			{
-				return "Trainer";
-			}
-			return Preferences.Get(key, "Trainer");
-		}
+    public static class PreferencesHelper
+    {
+        public static string GetSetting(string key)
+        {
+            //This is a wrapper for supporting unit tests
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                return "Trainer";
+            }
+            return Preferences.Get(key, "Trainer");
+        }
 
-		public static void SetSetting(string key, string value)
-		{
-			//This is a wrapper for supporting unit tests
-			if (DeviceInfo.Platform == DevicePlatform.Unknown)
-			{
-				return;
-			}
-			Preferences.Set(key, value);
-		}
-	}
+        public static void SetSetting(string key, string value)
+        {
+            //This is a wrapper for supporting unit tests
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                return;
+            }
+            Preferences.Set(key, value);
+        }
+    }
 }
