1b94d61009bcd323a7beec45442f7982f41ee0ae
PinKushin <PinKushin@verizon.net>
2025-03-14 02:34:57 -0400

    Refactor TrainerPageViewModel for improved properties
    Updated TrainerPageViewModel to include new observable properties: WelcomeMsg, Wins, Losses, Ties, and WinAverage. Removed previous implementations and initialized WelcomeMsg in the constructor. Refined CalculateWinRateAsync to use a switch statement for tallying match results and integrated LoadMatches functionality into it for asynchronous processing.

--- PokemonBattleJournal/ViewModels/TrainerPageViewModel.cs
+++ PokemonBattleJournal/ViewModels/TrainerPageViewModel.cs
@@ -1,72 +1,79 @@
-﻿using Newtonsoft.Json;
-
-namespace PokemonBattleJournal.ViewModels
+﻿namespace PokemonBattleJournal.ViewModels
 {
-	public partial class TrainerPageViewModel : ObservableObject
-	{
-		// get default file path
-		private static readonly string filePath = FileHelper.GetAppDataPath() + $"\\{PreferencesHelper.GetSetting("TrainerName")}.json";
+    public partial class TrainerPageViewModel : ObservableObject
+    {
+        // get default file path
+        //private static readonly string filePath = FileHelper.GetAppDataPath() + $"\\{PreferencesHelper.GetSetting("TrainerName")}.json";
 
-		[ObservableProperty]
-		public partial string TrainerName { get; set; } = PreferencesHelper.GetSetting("TrainerName");
+        [ObservableProperty]
+        public partial string TrainerName { get; set; } = PreferencesHelper.GetSetting("TrainerName");
 
-		[ObservableProperty]
-		public partial string WelcomeMsg { get; set; }
+        [ObservableProperty]
+        public partial string WelcomeMsg { get; set; }
 
-		[ObservableProperty]
-		public partial uint Wins { get; set; } = 0;
+        [ObservableProperty]
+        public partial uint Wins { get; set; } = 0;
 
-		[ObservableProperty]
-		public partial uint Losses { get; set; } = 0;
+        [ObservableProperty]
+        public partial uint Losses { get; set; } = 0;
 
-		[ObservableProperty]
-		public partial uint Ties { get; set; } = 0;
+        [ObservableProperty]
+        public partial uint Ties { get; set; } = 0;
 
-		[ObservableProperty]
-		public partial double WinAverage { get; set; } = 0;
+        [ObservableProperty]
+        public partial double WinAverage { get; set; } = 0;
 
-		public TrainerPageViewModel()
-		{
-			WelcomeMsg = $"{TrainerName}'s Profile";
-			LoadMatches();
-		}
+        public TrainerPageViewModel()
+        {
+            WelcomeMsg = $"{TrainerName}'s Profile";
+            //LoadMatches();
+            //CalculateWinRateAsync([]);
+        }
 
-		private void LoadMatches()
-		{
-			// create file if it doesn't exist
-			if (!FileHelper.Exists(filePath))
-			{
-				FileHelper.CreateFile(filePath);
-			}
-			//throws error if file doesn't exist so it was created above
-			string? saveFile = File.ReadAllText(filePath);
-			//deserialize file to add the new match or create an empty list of matches if no matches exist
-			List<MatchEntry> matchList = JsonConvert.DeserializeObject<List<MatchEntry>>(saveFile)
-				?? [];
-			//Tally up the wins, losses, and ties
-			foreach (MatchEntry match in matchList)
-			{
-				if (match.Result == "Win")
-				{
-					Wins++;
-				}
-				else if (match.Result == "Tie")
-				{
-					Ties++;
-				}
-				else
-				{
-					Losses++;
-				}
-			}
-			if (Wins + Losses + Ties == 0)
-			{
-				WinAverage = 0;
-			}
-			else
-			{
-				WinAverage = ((Wins + (0.5 * Ties)) / (Wins + Losses + Ties)) * 100;
-			}
-		}
-	}
+        /// <summary>
+        /// Calculate the average win rate of the trainer
+        /// using ((Wins + (0.5 * Ties)) / TotalGames * 100
+        /// </summary>
+        /// <param name="matchList">List of Matches</param>
+        public async Task<double> CalculateWinRateAsync(List<MatchEntry> matchList)
+        {
+            uint wins = 0;
+            uint losses = 0;
+            uint ties = 0;
+            double winRate = 0;
+
+            await Task.Run(() =>
+            {
+                foreach (MatchEntry match in matchList)
+                {
+                    switch (match.Result)
+                    {
+                        case MatchResult.Win:
+                            wins++;
+                            break;
+                        case MatchResult.Tie:
+                            ties++;
+                            break;
+                        case null:
+                            break;
+                        default:
+                            losses++;
+                            break;
+                    }
+                }
+                Wins = wins;
+                Losses = losses;
+                Ties = ties;
+                if (wins + losses + ties == 0)
+                {
+                    winRate = 0;
+                }
+                else
+                {
+                    winRate = ((wins + (0.5 * ties)) / (wins + losses + ties)) * 100;
+                }
+            });
+            return winRate;
+        }
+    }
 }
