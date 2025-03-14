24de5e5baa5332df6d1a1bbf0c60c24967a30c95
PinKushin <PinKushin@verizon.net>
2025-03-14 02:34:17 -0400

    Refactor TestPageViewModel for improved readability
    Reformatted `TestPageViewModel.cs` to enhance code structure.
    Removed unnecessary using directives and adjusted indentation
    for the class definition and `DeleteFile` method, ensuring
    consistent formatting while preserving functionality.

--- PokemonBattleJournal/ViewModels/TestPageViewModel.cs
+++ PokemonBattleJournal/ViewModels/TestPageViewModel.cs
@@ -1,35 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO.Enumeration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-
-namespace PokemonBattleJournal.ViewModels
+﻿namespace PokemonBattleJournal.ViewModels
 {
-	public partial class TestPageViewModel : ObservableObject
-	{
-		// get default file path
-		private static readonly string filePath = $@"{FileHelper.GetAppDataPath()}\{PreferencesHelper.GetSetting("TrainerName")}.json";
-		[ObservableProperty]
-		public partial string FileSuccessMessage { get; set; } = $"Delete {PreferencesHelper.GetSetting("TrainerName")}'s Trainer File?";
+    public partial class TestPageViewModel : ObservableObject
+    {
+        // get default file path
+        private static readonly string filePath = $@"{FileHelper.GetAppDataPath()}\{PreferencesHelper.GetSetting("TrainerName")}.json";
+        [ObservableProperty]
+        public partial string FileSuccessMessage { get; set; } = $"Delete {PreferencesHelper.GetSetting("TrainerName")}'s Trainer File?";
 
-		[RelayCommand]
-		public void DeleteFile()
-		{
+        [RelayCommand]
+        public void DeleteFile()
+        {
 
-			// Delete Trainer Match file if it exists
-			if (FileHelper.Exists(filePath))
-			{
-				FileHelper.DeleteFile(filePath);
-				FileSuccessMessage = "File Deleted";
-			}
-			else
-			{
-				FileSuccessMessage = "File Not Found";
-			}
-		}
-	}
+            // Delete Trainer Match file if it exists
+            if (FileHelper.Exists(filePath))
+            {
+                FileHelper.DeleteFile(filePath);
+                FileSuccessMessage = "File Deleted";
+            }
+            else
+            {
+                FileSuccessMessage = "File Not Found";
+            }
+        }
+    }
 }
