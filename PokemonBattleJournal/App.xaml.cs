4ff511b6f97bfe39e28fab3a5397e9604ad30a53
PinKushin <PinKushin@verizon.net>
2025-03-14 02:42:39 -0400

    Enhance App class with logging and view model support
    Updated the `App` class in `App.xaml.cs` to implement dependency injection for logging by adding an `ILogger<App>` parameter to the constructor.
    The `CreateWindow` method now creates `FirstStartPage` with a new `FirstStartPageViewModel`, improving code clarity and structure.

--- PokemonBattleJournal/App.xaml.cs
+++ PokemonBattleJournal/App.xaml.cs
@@ -1,21 +1,30 @@
-﻿namespace PokemonBattleJournal
-{
-	public partial class App : Application
-	{
-		public App()
-		{
-			//Register Syncfusion license
-			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzY3NjYwMkAzMjM4MmUzMDJlMzBuWkMvdUxhYkNPWERMQndYazNyU1gzWnVoN29Zb2dxU1AxQTk2K0k3aXNFPQ==");
-			InitializeComponent();
-		}
+﻿using Microsoft.Extensions.Logging;
 
-		protected override Window CreateWindow(IActivationState? activationState)
-		{
-			if (PreferencesHelper.GetSetting("FirstStart") != "false")
-			{
-				return new Window(new FirstStartPage(new()));
-			}
-			return new Window(new AppShell());
-		}
-	}
+namespace PokemonBattleJournal
+{
+
+    public partial class App : Application
+    {
+        private readonly ILogger<App> _logger;
+
+        public App(ILogger<App> logger)
+        {
+            //Register Syncfusion license
+            Syncfusion
+                .Licensing
+                .SyncfusionLicenseProvider
+                .RegisterLicense(
+                "MzY3NjYwMkAzMjM4MmUzMDJlMzBuWkMvdUxhYkNPWERMQndYazNyU1gzWnVoN29Zb2dxU1AxQTk2K0k3aXNFPQ=="
+                );
+            _logger = logger;
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            if (PreferencesHelper.GetSetting("FirstStart") != "false")
+                return new Window(new FirstStartPage(new FirstStartPageViewModel()));
+            return new Window(new AppShell());
+        }
+    }
 }
