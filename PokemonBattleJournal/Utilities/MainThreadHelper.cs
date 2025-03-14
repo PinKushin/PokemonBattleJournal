1d06a3084bda53027f3ba22068235c7feae9f215
PinKushin <PinKushin@verizon.net>
2025-03-14 02:26:42 -0400

    Refactor MainThreadHelper for readability
    Reformatted the `MainThreadHelper` class in `MainThreadHelper.cs` to improve consistency and clarity. Added whitespace for better readability while preserving the existing logic and functionality of the `BeginInvokeOnMainThread` and `IsMainThread` methods. The overall structure remains intact, maintaining its role as a wrapper for supporting unit tests.

--- PokemonBattleJournal/Utilities/MainThreadHelper.cs
+++ PokemonBattleJournal/Utilities/MainThreadHelper.cs
@@ -1,29 +1,29 @@
 ﻿namespace PokemonBattleJournal.Utilities
 {
-	public static class MainThreadHelper
-	{
-		public static void BeginInvokeOnMainThread(Action action)
-		{
-			//This is a wrapper for supporting unit tests
-			if (DeviceInfo.Platform == DevicePlatform.Unknown)
-			{
-				_ = Task.Run(action);
-				return;
-			}
-			MainThread.BeginInvokeOnMainThread(action);
-		}
+    public static class MainThreadHelper
+    {
+        public static void BeginInvokeOnMainThread(Action action)
+        {
+            //This is a wrapper for supporting unit tests
+            if (DeviceInfo.Platform == DevicePlatform.Unknown)
+            {
+                _ = Task.Run(action);
+                return;
+            }
+            MainThread.BeginInvokeOnMainThread(action);
+        }
 
-		public static bool IsMainThread
-		{
-			get
-			{
-				//This is a wrapper for supporting unit tests
-				if (DeviceInfo.Platform == DevicePlatform.Unknown)
-				{
-					return true;
-				}
-				return MainThread.IsMainThread;
-			}
-		}
-	}
+        public static bool IsMainThread
+        {
+            get
+            {
+                //This is a wrapper for supporting unit tests
+                if (DeviceInfo.Platform == DevicePlatform.Unknown)
+                {
+                    return true;
+                }
+                return MainThread.IsMainThread;
+            }
+        }
+    }
 }
