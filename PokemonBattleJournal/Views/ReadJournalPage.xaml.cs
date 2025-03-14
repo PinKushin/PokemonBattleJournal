93eb0fc5ada31b8c3db71e27d46e7c49191222f4
PinKushin <PinKushin@verizon.net>
2025-03-14 02:46:12 -0400

    Refactor ReadJournalPage constructor formatting
    Removed unnecessary whitespace and improved formatting in the constructor of the `ReadJournalPage` class. The functionality remains unchanged, ensuring proper initialization of the component and binding context.

--- PokemonBattleJournal/Views/ReadJournalPage.xaml.cs
+++ PokemonBattleJournal/Views/ReadJournalPage.xaml.cs
@@ -2,9 +2,9 @@ namespace PokemonBattleJournal.Views;
 
 public partial class ReadJournalPage : ContentPage
 {
-	public ReadJournalPage(ReadJournalPageViewModel vm)
-	{
-		InitializeComponent();
-		BindingContext = vm;
-	}
+    public ReadJournalPage(ReadJournalPageViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
 }
