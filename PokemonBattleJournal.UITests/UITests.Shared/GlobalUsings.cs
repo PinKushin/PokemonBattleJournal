7aa19581923b6219d0b443216f469a4022dc66b0
pinkushin <pinkushin@verizon.net>
2025-03-28 16:44:33 -0400

    Implement Appium testing setup for Android and Windows
    Updated the codebase to introduce a new Appium testing setup, managing the Appium driver and server lifecycle with the `AppiumSetup` and `AppiumServerHelper` classes. Project files for Android and Windows now target .NET 9.0 and include necessary package references. Added new test classes (`AppWindowTests` and `MainPageTests`) to demonstrate the testing framework. Global usings have been added for code simplification, and the `BaseTest` class has been modified to ensure the Appium server is set up once for all tests in the collection.

---
+++ PokemonBattleJournal.UITests/UITests.Shared/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using OpenQA.Selenium.Appium;
+global using OpenQA.Selenium.Appium.Android;
+global using OpenQA.Selenium.Appium.Enums;
+global using OpenQA.Selenium.Appium.Service;
+global using OpenQA.Selenium.Appium.Windows;
+global using Shouldly;
+global using Xunit;
---
+++ PokemonBattleJournal.UITests/UITests.Shared/Views/MainPageTests.cs
@@ -0,0 +1,56 @@
+﻿namespace UITests
+{
+    public class MainPageTests : BaseTest
+    {
+        [Fact]
+        public void MainPage_UserNoteInput_ShowTextEntry()
+        {
+            // Arrange
+            AppiumElement userEntry = FindUIElement("UserNoteInput");
+
+            // Act
+            userEntry.SendKeys("Hello World");
+
+            // Assert
+            userEntry.ShouldNotBeNull();
+            userEntry.Text.ShouldBe("Hello World");
+
+        }
+
+        [Fact]
+        public async Task MainPage_BOSwitch_DisplayedAndToggled()
+        {
+            // Arrange
+            AppiumElement BOSwitch = FindUIElement("BOSwitch");
+            CancellationToken cancellationToken = new();
+
+            // Act
+            BOSwitch.Click();
+            await Task.Delay(500).WaitAsync(cancellationToken); // Wait for the click to register 
+            var toggledOn = BOSwitch.GetAttribute("Toggle.ToggleState");
+            BOSwitch.Click();
+            await Task.Delay(500).WaitAsync(cancellationToken); // Wait for the click to register 
+            var toggledOff = BOSwitch.GetAttribute("Toggle.ToggleState");
+            // Assert
+            BOSwitch.ShouldNotBeNull();
+            BOSwitch.Displayed.ShouldBeTrue();
+            BOSwitch.Enabled.ShouldBeTrue();
+            toggledOn.ShouldBe("1"); // "1" for toggled on
+            toggledOff.ShouldBe("0");// "0" for toggled off
+
+        }
+
+        [Fact]
+        public void MainPage_BallIcon_DisplayedOnPage()
+        {
+            // Arrange
+            AppiumElement BallIconPng = FindUIElement("ball_icon.png");
+            // Act
+            // Assert
+
+            BallIconPng.ShouldNotBeNull();
+            BallIconPng.Displayed.ShouldBeTrue();
+            BallIconPng.Enabled.ShouldBeTrue();
+        }
+    }
+}
