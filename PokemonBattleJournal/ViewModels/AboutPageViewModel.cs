3a8c463af295ce7e5e2a0c50b4ef8e18596fd1cb
pinkushin <pinkushin@verizon.net>
2025-03-28 05:55:17 -0400

    Add logging support to AboutPageViewModel
    - Introduced a private readonly field `_logger` of type `ILogger<AboutPageViewModel>`.
    - Added a constructor to initialize the `_logger` field.

--- PokemonBattleJournal/ViewModels/AboutPageViewModel.cs
+++ PokemonBattleJournal/ViewModels/AboutPageViewModel.cs
@@ -2,5 +2,10 @@
 {
     public partial class AboutPageViewModel : ObservableObject
     {
+        private readonly ILogger<AboutPageViewModel> _logger;
+        public AboutPageViewModel(ILogger<AboutPageViewModel> logger)
+        {
+            _logger = logger;
+        }
     }
 }
