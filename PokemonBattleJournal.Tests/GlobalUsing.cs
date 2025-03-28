0df9d360cf5d5ff826d5dcff474cdcfbbf6530bb
pinkushin <pinkushin@verizon.net>
2025-03-28 16:42:47 -0400

    Add global usings for NSubstitute and Services
    This commit introduces two new global using directives:
    `NSubstitute` and `PokemonBattleJournal.Services`.
    These additions simplify the usage of types from these namespaces
    across the project by eliminating the need to specify them
    in each individual file.

--- PokemonBattleJournal.Tests/GlobalUsing.cs
+++ PokemonBattleJournal.Tests/GlobalUsing.cs
@@ -1,5 +1,7 @@
 ﻿global using Microsoft.Extensions.Logging;
+global using NSubstitute;
 global using PokemonBattleJournal.Models;
+global using PokemonBattleJournal.Services;
 global using PokemonBattleJournal.ViewModels;
 global using Shouldly;
 global using Xunit;
