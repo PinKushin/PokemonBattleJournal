1bb57c0d05f41bfe412921527b02f911e4a0a08e
pinkushin <pinkushin@verizon.net>
2025-03-28 05:56:38 -0400

    Refactor logging in FirstStartPageViewModel
    Removed unused logging directive and added logger instance.
    Updated logging statement to ensure trainer's name is logged
    after the conditional block.

--- PokemonBattleJournal/ViewModels/FirstStartPageViewModel.cs
+++ PokemonBattleJournal/ViewModels/FirstStartPageViewModel.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace PokemonBattleJournal.ViewModels
+﻿namespace PokemonBattleJournal.ViewModels
 {
     public partial class FirstStartPageViewModel : ObservableObject
     {
@@ -14,13 +12,13 @@ namespace PokemonBattleJournal.ViewModels
         public void SaveTrainerName()
         {
             var logger = new Logger<FirstStartPageViewModel>(new LoggerFactory());
-            logger.LogInformation("Created Trainer: {Name}", TrainerNameInput);
             if (TrainerNameInput != null && Application.Current != null)
             {
                 PreferencesHelper.SetSetting("FirstStart", "false");
                 PreferencesHelper.SetSetting("TrainerName", TrainerNameInput);
                 Application.Current.Windows[0].Page = new AppShell();
             }
+            logger.LogInformation("Created Trainer: {Name}", TrainerNameInput);
         }
     }
 }
