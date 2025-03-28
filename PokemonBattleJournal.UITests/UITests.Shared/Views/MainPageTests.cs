04ef58d10afd149c07d72226c0c94a6aa164c966
pinkushin <pinkushin@verizon.net>
2025-03-28 17:58:46 -0400

    Refactor MainPageTests for async support and cleanup
    - Changed `MainPageTests` to a partial class for better organization.
    - Updated `MainPage_UserNoteInput_ShowTextEntry` to be asynchronous.
    - Introduced a `CancellationToken` for task management.
    - Added a delay with cancellation support in the user note input test.
    - Removed the `MainPage_BOSwitch_DisplayedAndToggled` test method.

--- PokemonBattleJournal.UITests/UITests.Shared/Views/MainPageTests.cs
+++ PokemonBattleJournal.UITests/UITests.Shared/Views/MainPageTests.cs
@@ -1,42 +1,20 @@
 ﻿namespace UITests
 {
-    public class MainPageTests : BaseTest
+    public partial class MainPageTests : BaseTest
     {
         [Fact]
-        public void MainPage_UserNoteInput_ShowTextEntry()
+        public async Task MainPage_UserNoteInput_ShowTextEntry()
         {
             // Arrange
             AppiumElement userEntry = FindUIElement("UserNoteInput");
-
+            CancellationToken cancellationToken = new();
             // Act
             userEntry.SendKeys("Hello World");
+            await Task.Delay(500).WaitAsync(cancellationToken);
 
             // Assert
             userEntry.ShouldNotBeNull();
             userEntry.Text.ShouldBe("Hello World");
-
-        }
-
-        [Fact]
-        public async Task MainPage_BOSwitch_DisplayedAndToggled()
-        {
-            // Arrange
-            AppiumElement BOSwitch = FindUIElement("BOSwitch");
-            CancellationToken cancellationToken = new();
-
-            // Act
-            BOSwitch.Click();
-            await Task.Delay(500).WaitAsync(cancellationToken); // Wait for the click to register 
-            var toggledOn = BOSwitch.GetAttribute("Toggle.ToggleState");
-            BOSwitch.Click();
-            await Task.Delay(500).WaitAsync(cancellationToken); // Wait for the click to register 
-            var toggledOff = BOSwitch.GetAttribute("Toggle.ToggleState");
-            // Assert
-            BOSwitch.ShouldNotBeNull();
-            BOSwitch.Displayed.ShouldBeTrue();
-            BOSwitch.Enabled.ShouldBeTrue();
-            toggledOn.ShouldBe("1"); // "1" for toggled on
-            toggledOff.ShouldBe("0");// "0" for toggled off
 
         }
 
