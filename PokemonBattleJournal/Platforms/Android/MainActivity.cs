24de5bc794dca8991325dd8a5d234eff96d91ff6
pinkushin <pinkushin@verizon.net>
2025-03-28 16:46:04 -0400

    Add Android.Runtime import and [Register("com.PinKushin.PokemonBattleJournal.MainActivity")]
    This commit adds an import statement for `Android.Runtime` in `MainActivity.cs` to enable the runtime feature [Register("com.PinKushin.PokemonBattleJournal.MainActivity")]

--- PokemonBattleJournal/Platforms/Android/MainActivity.cs
+++ PokemonBattleJournal/Platforms/Android/MainActivity.cs
@@ -1,9 +1,11 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Runtime;
 
 namespace PokemonBattleJournal.Platforms.Android
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Register("com.PinKushin.PokemonBattleJournal.MainActivity")]
     public class MainActivity : MauiAppCompatActivity
     {
     }
